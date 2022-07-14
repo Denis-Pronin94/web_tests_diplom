86715d4648653349191ea60668803f5f392215ae
dddpr <ddd.pronin@yandex.ru>
2022-07-14 12:01:21 +0300

    fix

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -9,7 +9,6 @@ import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import pages.MainPage;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.sleep;
